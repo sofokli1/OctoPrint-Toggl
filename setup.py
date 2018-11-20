1bef8f7b3efa6db5e9c01e56de6710fc8140b761
Juxhin (Eugene) Bakalli <sofokli1@gmail.com>
2018-11-20 20:51:41 +0200

    Remove togglPy

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/sofokli1/OctoPrint-Toggl"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ["TogglPy"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
