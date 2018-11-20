79a0c509aacb202c643bdbc6067a212f638a3d13
Juxhin (Eugene) Bakalli <sofokli1@gmail.com>
2018-11-20 20:38:08 +0200

    Copy dependency into project

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/sofokli1/OctoPrint-Toggl"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["TogglPy"]
+plugin_requires = []
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
